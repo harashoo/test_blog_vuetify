fbc9846b910f3136674a85c6e838772e3379d691
shogo-hr <harada.tosho.2016@gmail.com>
2020-11-15 21:50:35 +0900

    記事の詳細を追加

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-container>
+      <v-app-bar color="#FFFFFF" flat max-height="100" class="mb-5">
+        <v-toolbar-title>
+          <h1><router-link to="/" class="logo">ブログ！</router-link></h1>
+        </v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <div id="nav">
+          <router-link to="/">Home</router-link> |
+          <router-link to="/about">About</router-link>
+        </div>
+      </v-app-bar>
+
+      <router-view />
+    </v-container>
+  </v-app>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  a {
+    color: #2c3e50;
+    text-decoration: none;
+  }
 }
 
 #nav {
   padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  .router-link-exact-active {
+    color: #42b983;
   }
 }
 </style>
