cf55a739a732e199eaae36694cb9121a223bc073
shogo-hr <harada.tosho.2016@gmail.com>
2020-11-16 23:42:59 +0900

    記事の詳細を取得する

--- src/views/ArticleDetail.vue
+++ src/views/ArticleDetail.vue
@@ -1,22 +1,34 @@
 <template>
   <v-card flat>
-    <v-img
-      height="400"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-    ></v-img>
+    <v-img height="400" :src="article.image.url"></v-img>
 
-    <v-card-title>Top 10 Australian beaches</v-card-title>
+    <v-card-title>{{ article.title }}</v-card-title>
 
     <v-card-text>
-      <div>Whitehaven Beach</div>
-
-      <div>Whitsunday Island, Whitsunday Islands</div>
+      <div>{{ article.body }}</div>
     </v-card-text>
   </v-card>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "ArticleDetail"
+  name: "ArticleDetail",
+
+  data: () => ({
+    article: {}
+  }),
+
+  async mounted() {
+    const response = await axios.get(
+      "https://my-blog-harasho.microcms.io/api/v1/articles/" +
+        this.$route.params.id,
+      {
+        headers: { "X-API-KEY": process.env.VUE_APP_X_API_KEY }
+      }
+    );
+    this.article = response.data;
+  }
 };
 </script>
